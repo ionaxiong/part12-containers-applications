46d9139ab7cda9df06e872fa6e3959724cf1ee3d
ming xiong <iona.m.xiong@gmail.com>
2024-01-16 22:09:30 +0200

    12.18

--- todo-app/todo-backend/app.js
+++ todo-app/todo-backend/app.js
@@ -3,8 +3,8 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const indexRouter = require("./routes/index");
-const todosRouter = require("./routes/todos");
-const statisticsRouter = require("./routes/statistics");
+// const todosRouter = require("./routes/todos");
+// const statisticsRouter = require("./routes/statistics");
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(logger("dev"));
 app.use(express.json());
 
 app.use("/", indexRouter);
-app.use("/todos", todosRouter);
-app.use("/statistics", statisticsRouter);
+// app.use("/todos", todosRouter);
+// app.use("/statistics", statisticsRouter);
 
 module.exports = app;

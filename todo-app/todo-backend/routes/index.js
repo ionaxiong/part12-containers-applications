402f3bb224391889327baf516e9458727575e9a7
ming xiong <iona.m.xiong@gmail.com>
2024-01-17 14:23:43 +0200

    12.19

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const router = express.Router();
-// const redis = require("../redis");
-// const configs = require("../util/config");
+const redis = require("../redis");
+const configs = require("../util/config");
 
 let visits = 0;
 
 /* GET index data. */
 router.get("/", async (req, res) => {
-  res.send("Hello World!");
-  // visits++;
-  // await redis.setAsync("visits", visits);
-  // res.send({
-  //   ...configs,
-  //   visits,
-  // });
+  // res.send("Hello World!");
+  visits++;
+  await redis.setAsync("visits", visits);
+  res.send({
+    ...configs,
+    visits,
+  });
 });
 
-router.get("/todos", async (req, res) => {
-  res.send("Hello World!");
-});
+// router.get("/todos", async (req, res) => {
+//   res.send("Hello World!");
+// });
 
 module.exports = router;
--- todo-app/todo-frontend/dev.Dockerfile
+++ todo-app/todo-frontend/dev.Dockerfile
@@ -8,6 +8,7 @@ COPY . .
 RUN npm install
 
 # ENV REACT_APP_BACKEND_URL=http://localhost:3000
+ENV REACT_APP_BACKEND_URL=/api
 
 # npm start is the command to start the application in development mode
 CMD ["npm", "start"]
